3b0c985ae679c6bbcc4559010186c50516d8845b
Eduardo A.F <eacevedof@yahoo.es>
2018-12-01 14:38:38 +0100

    hace faker

--- backend/src/tests/InsertsTest.php
+++ backend/src/tests/InsertsTest.php
@@ -3,29 +3,24 @@
 // ./vendor/bin/phpunit --bootstrap ./vendor/theframework/bootstrap.php ./src/tests/InsertsTest.php --color=auto
 // ./vendor/bin/phpunit --bootstrap ./vendor/theframework/bootstrap.php ./src/tests
 use PHPUnit\Framework\TestCase;
-use TheFramework\Components\ComponentLog;
-
+use App\Traits\AppLogTrait;
 use App\Services\RamdomizerService;
+//models
 use App\Models\DepartmentModel;
 use App\Models\DeptEmpModel;
 use App\Models\EmployeeModel;
 use App\Models\SalaryModel;
 use App\Models\TitleModel;
 
-
 class InsertsTest extends TestCase
 {    
+    use AppLogTrait;
+    
     private $oRnd;
     
     public function __construct() {
         parent::__construct();
         $this->oRnd = new RamdomizerService();
-    }
-    
-    private function log($mxVar,$sTitle=NULL)
-    {
-        $oLog = new ComponentLog("logs",__DIR__);
-        $oLog->save($mxVar,$sTitle);
     }
     
     private function get_employee_fakedata()
@@ -45,30 +40,45 @@ class InsertsTest extends TestCase
         $oEmployee = new EmployeeModel();
         //hace un select distinct de gender
         $arGender = $oEmployee->get_gender();
-        $iMax = (int) $oEmployee->get_max("emp_no");
+        $arGender = array_column($arGender,"gender");
+        $iMax = (int)$oEmployee->get_max("emp_no");
+        $iMax++;
         //emulo el post
-        $arPost["empno"] = ($iMax++);
+        $arPost["empno"] = $iMax;
         $arPost["birthdate"] = $this->oRnd->get_date_ymd();
-        $arPost["firstname"] = $this->oRnd->get_date_ymd();
-        $arPost["lastname"] = $this->oRnd->get_date_ymd();
+        $arPost["firstname"] = $this->oRnd->get_substring_len(14);
+        $arPost["lastname"] = $this->oRnd->get_substring_len(16);
         $arPost["gender"] = $this->oRnd->get_itemval($arGender);
         $arPost["hiredate"] = $this->oRnd->get_date_ymd();
         
         return $arPost;
     }
     
-    public function insert_simple_employee()
+    private function employee_insert()
     {
+        $arPost = $this->get_employee_fakedata();
+        $this->logd($arPost);
         $oEmployee = new EmployeeModel();
         //hago el insert del empleado
         $oEmployee->insert($arPost);
+        return $oEmployee;
+    }
+
+    public function test_insert_simple_employee()
+    {
+        $oEmployee = $this->employee_insert();
+        $this->assertEquals(FALSE,$oEmployee->is_error());
     }
     
-    public function test_multiple_inserts()
+    public function test_insert_multiple_employees()
     {
-        for($i=0;$i<10;$i++)
+        $iNum = $this->oRnd->get_int(2,10);
+        $this->logd("multiple: inum: $iNum");
+        
+        for($i=0;$i<$iNum;$i++)
         {
-            
+            $oEmployee = $this->employee_insert();
+            $this->assertEquals(FALSE,$oEmployee->is_error());
         }
     }
       
