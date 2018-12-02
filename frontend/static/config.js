ef7a82a8b0813289af87d10dbb88c71f171ac101
Eduardo A.F <eacevedof@yahoo.es>
2018-12-02 20:21:41 +0100

    ya funciona el front

--- frontend/static/config.js
+++ frontend/static/config.js
@@ -1,5 +1,5 @@
 const oConfig = {
-    endpoint: "http://localhost:3000/",
+    //endpoint: "http://localhost:3000/",
     endpoint: "http://telecoming.eduardoaf.com/"
 }
-console.log(oConfig.endpoint,"oConfig.endpoint:");
+//console.log(oConfig.endpoint,"oConfig.endpoint:");
